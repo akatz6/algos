987a07f357bbba2c5495ab4a75bd28e74afa47c0
akatz6 <aaronk488@gmail.com>
2024-06-28 20:17:47 -0700

    another solution for longest substring

--- sliding_window/longestSubstring.js
+++ sliding_window/longestSubstring.js
@@ -1,21 +1,20 @@
 const longestSubString = (str) => {
-  if(!str.length) return 0
-  if(!str.length) return 0
-  const obj = {}
-  const len = [0,0]
-  let left = right = 0;
-  while(right < str.length){
-      const letter = str[right]
-      while(letter in obj){
-          delete obj[str[left]]
-          left++
-      }
-      obj[letter] = letter
-      if((right - left) > (len[1] - len[0])){
-          len[1] = right
-          len[0] = left
-      }
-      right++;
+//   if(!str.length) return 0
+//   const obj = {}
+//   const len = [0,0]
+//   let left = right = 0;
+//   while(right < str.length){
+//       const letter = str[right]
+//       while(letter in obj){
+//           delete obj[str[left]]
+//           left++
+//       }
+//       obj[letter] = letter
+//       if((right - left) > (len[1] - len[0])){
+//           len[1] = right
+//           len[0] = left
+//       }
+//       right++;
   // if (!str.length) return 0;
   // const set = new Set();
   // let left = 0;
@@ -33,6 +32,24 @@ const longestSubString = (str) => {
   //   right++;
   // }
   // return len;
+  const obj = {}
+  const arr = []
+  let len = 0;
+  for(let i = 0; i < str.length; i++){
+      const letter = str[i]
+      if(letter in obj){
+          while(arr[0] !== letter){
+              const removeLetter = arr.shift()
+              delete obj[removeLetter]
+          }
+          arr.shift()
+          delete obj[letter]
+      }
+      obj[letter] = i
+      arr.push(letter)
+      len = Math.max(arr.length, len)
+  }
+  return len;
 };
 
 // console.log(longestSubString("abcabcbb"));
